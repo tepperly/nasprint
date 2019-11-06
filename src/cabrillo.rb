86774780bd83a59528ecb062e99b73f448cc0cc7
Tom Epperly <tepperly@gmail.com>
2019-11-06 00:53:26 +0000

    Checkpointing

--- src/cabrillo.rb
+++ src/cabrillo.rb
@@ -7,8 +7,8 @@
 require 'csv'
 require 'time'
 
-CONTEST_START=Time.utc(2018,4,8,0,0)
-CONTEST_END=Time.utc(2018,4,8,4,0)
+CONTEST_START=Time.utc(2019,3,24,0,0)
+CONTEST_END=Time.utc(2019,3,24,4,0)
 
 def mySplit(str, pattern)
   result = [ ]
