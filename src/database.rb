fba050bc30f5ab1ccb2ff48c9f4e45d689842634
Tom Epperly <tepperly@gmail.com>
2024-09-04 23:57:08 +0000

    Look for a configuration file for the username, password, host, and database name.

--- src/database.rb
+++ src/database.rb
@@ -5,9 +5,37 @@
 #
 
 require 'mysql2'
+require 'json'
 
 def makeDB
-  return Mysql2::Client.new(:host => "localhost",
-                            :username => "nasprint", :password => "NASpRiNtpwd",
-                            :database => "NASprint2")
+  host = "localhost"
+  username = "nasprint"
+  database = "NASprint"
+  password = "secret"
+  [ Dir.home, Dir.getwd ].each { |directory|
+    filename = File.join(directory, ".nasprint.json")
+    if File.file?(filename)
+      begin
+        o = JSON.parse(File.read(filename))
+        if o.has_key?("username")
+          username = o["username"]
+        end
+        if o.has_key?("database")
+          database = o["database"]
+        end
+        if o.has_key?("password")
+          password = o["password"]
+        end
+        if o.has_key?("host")
+          host = o["host"]
+        end
+        break
+      rescue
+        print "Unable to read in file " + filename +  "\n"
+      end
+    end
+  }
+  return Mysql2::Client.new(:host => host,
+                            :username => username, :password => password,
+                            :database => database)
 end
