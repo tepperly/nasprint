433ac4e10d6bf0eff1ebc5c64b19697b01f2a300
Tom Epperly <tepperly@gmail.com>
2024-09-05 00:09:14 +0000

    Improve implementation

--- src/database.rb
+++ src/database.rb
@@ -8,34 +8,24 @@ require 'mysql2'
 require 'json'
 
 def makeDB
-  host = "localhost"
-  username = "nasprint"
-  database = "NASprint"
-  password = "secret"
+  configuration={"host" => "localhost", "username" => "nasprint", "database" => "NASprint", "password" => "secret" }
   [ Dir.home, Dir.getwd ].each { |directory|
     filename = File.join(directory, ".nasprint.json")
     if File.file?(filename)
       begin
         o = JSON.parse(File.read(filename))
-        if o.has_key?("username")
-          username = o["username"]
-        end
-        if o.has_key?("database")
-          database = o["database"]
-        end
-        if o.has_key?("password")
-          password = o["password"]
-        end
-        if o.has_key?("host")
-          host = o["host"]
-        end
+        configuration.keys.each{ |key|
+          if o.has_key?(key)
+            configuration[key] = o[key]
+          end
+        }
         break
       rescue
         print "Unable to read in file " + filename +  "\n"
       end
     end
   }
-  return Mysql2::Client.new(:host => host,
-                            :username => username, :password => password,
-                            :database => database)
+  return Mysql2::Client.new(:host => configuration["host"],
+                            :username => configuration["username"], :password => configuration["password"],
+                            :database => configuration["database"])
 end
